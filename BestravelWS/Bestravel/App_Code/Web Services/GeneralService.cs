223f4c4b6271d56f34887a05e8089dceb6459c64
shir shahak <shahakshir@gmail.com>
2019-01-12 16:54:28 +0200

    .

--- BestravelWS/Bestravel/App_Code/Web Services/GeneralService.cs
+++ BestravelWS/Bestravel/App_Code/Web Services/GeneralService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GeneralService" in code, svc and config file together.
