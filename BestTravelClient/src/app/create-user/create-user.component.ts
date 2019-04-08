fae8a6e70ae702c4088d31ec507cca10d7a5cadb
Danaorlov1005 <dandik.o12@gmail.com>
2019-04-08 17:20:11 +0300

    create-user

--- BestTravelClient/src/app/create-user/create-user.component.ts
+++ BestTravelClient/src/app/create-user/create-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
@@ -6,12 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
-  componentproperty;
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
   ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6)]]
+      });
   }
-  onClickSubmit(data) {
-     alert("Entered Email id : " + data.emailid);
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      alert('SUCCESS!! :-)')
   }
 
 }
