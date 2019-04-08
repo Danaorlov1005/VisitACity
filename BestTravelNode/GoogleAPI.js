358a117d1c1225cc97a2820414a17dc80c67b676
shir shahak <shahakshir@gmail.com>
2019-04-08 20:59:56 +0300

    get places + placesTypes

--- BestTravelNode/GoogleAPI.js
+++ BestTravelNode/GoogleAPI.js
@@ -96,46 +96,5 @@ function PlaceResponse(response) {
     });
 }
 
-// getCoordinates(37202); //Enter a zip code here to try it out (Nashville in this case)
-
-
 module.exports = { placeSearch };
-
-
-
-
-
-
-// exports.randeats = function (req, res) {
-//     var key = req.query.key;
-//     var location = encodeURIComponent(req.query.location);
-//     var radius = 16000;
-//     var sensor = false;
-//     var types = "restaurant";
-//     var keyword = "fast";
-
-//     const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + 41.9038463 + "," + 12.4904047 + "&radius=500&type=bar&key=AIzaSyBts53vgjeOpiVy962cJUvS8D021tTgpdI";
-
-//     // var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types + "&keyword=" + keyword;
-
-//     console.log(url);
-//     https.get(url, function(response) {
-//       var body ='';
-//       response.on('data', function(chunk) {
-//         body += chunk;
-//     });
-
-//     response.on('end', function() {
-//         var places = JSON.parse(body);
-//         var locations = places.results;
-//         var randLoc = locations[Math.floor(Math.random() * locations.length)];
-
-//         res.json(randLoc);
-//     });
-
-
-// }).on('error', function(e) {
-//     console.log("Got error: " + e.message);
-// });
-// };
 
