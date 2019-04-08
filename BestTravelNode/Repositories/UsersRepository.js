f4bbbeeeadf4b4b597cfa2ff5b16ed8a2b21cd89
yarinan <yarin.andreev@cs-robotics.com>
2019-04-08 19:51:21 +0300

    sd

--- BestTravelNode/Repositories/UsersRepository.js
+++ BestTravelNode/Repositories/UsersRepository.js
@@ -15,4 +15,21 @@ function getTripsForUser(data){
     return executeQuery(query, values)
 }
 
-module.exports = {getTripsForUser}
+function addNewUser(data){
+    var query = `
+        INSERT INTO public."Users"
+        VALUES(nextval('users_seq'),$1,$2)
+    `
+    return executeQuery(query, [data.UserName, data.Password]);
+}
+
+function getUser(data){
+    let query = `
+    SELECT * 
+    FROM public."Users" u
+    WHERE u."UserName" = $1
+    AND u."Password" = $2
+` 
+    return executeQuery(query, [data.UserName, data.Password]);
+}
+module.exports = {getTripsForUser, addNewUser, getUser}
