6e660d3e1c66d4b81cd6de6db1e23932ff587ce4
yarin.andreev <yarin.andreev@cs-robotics.com>
2019-06-11 14:53:41 +0300

    get trip by user

--- BestTravelNode/Repositories/UsersRepository.js
+++ BestTravelNode/Repositories/UsersRepository.js
@@ -3,13 +3,9 @@ const { executeQuery } = require('../DBAccess')
 function getTripsForUser(data){
     let query =`
         SELECT t."id", t."locations", t."name", t."area", t."duration"
-        FROM public."TRIP_USER" tu,
-            public."Trips" t,
-            public."Users" u
-        WHERE tu."TRIP_ID" = t."id" AND
-              tu."USER_ID" = $1
+        FROM public."TRIPS" t,
+        WHERE t."USER_ID" = $1
     `
-    //const values=[data.userId]
     const values = [data.userId]
 
     return executeQuery(query, values)
